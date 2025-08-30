afcdd80a48c7c9d5adcb48e9c22d07b565bdf2f1
Navesh Jaiswal <navesh.professional@gmail.com>
2025-08-30 23:01:59 +0530

    Fix CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import reportRoutes from "./routes/reportRoutes.js";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
 
 app.use("/reports", reportRoutes);
 
