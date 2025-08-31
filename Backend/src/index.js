0fdf9226e3b00bc7ecc3560254c7c52ff1cba228
Navesh Jaiswal <navesh.professional@gmail.com>
2025-09-01 02:08:29 +0530

    Fix CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import cors from "cors";
 import reportRoutes from "./routes/reportRoutes.js";
+import path from "path";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-}));
+// CORS (allow frontend)
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
+// Serve temporary uploaded PDFs
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/reports", reportRoutes);
 
