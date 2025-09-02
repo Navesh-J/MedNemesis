39c1a6ac202ac372492c332b6a1face672fdcf0d
Navesh Jaiswal <navesh.professional@gmail.com>
2025-09-02 23:17:28 +0530

    fix cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS (allow frontend)
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://mednemesis.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
