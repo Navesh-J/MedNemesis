fa82f942eb8323024dc1233cb1dcb2865934623d
Navesh Jaiswal <navesh.professional@gmail.com>
2026-08-18 21:15:01 +0530

    initial backend setup

---
+++ Backend-Spring/src/main/java/com/spring/mednemesis/MednemesisApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mednemesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MednemesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MednemesisApplication.class, args);
+	}
+
+}
